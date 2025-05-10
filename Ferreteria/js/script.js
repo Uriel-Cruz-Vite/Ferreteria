96dc7d4ce5c06c00c4a902cffa3546246846bc4e
Uriel-Cruz-Vite <167963030+Uriel-Cruz-Vite@users.noreply.github.com>
2025-05-09 20:07:43 -0600

    Delete Ferreteria directory

--- Ferreteria/js/script.js
+++
@@ -1,8 +0,0 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel img');
-
-setInterval(() => {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-}, 5000);
